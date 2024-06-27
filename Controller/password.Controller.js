db500a2bffa0cdf01a3e7956ff6c325923b620f2
Prince Yadav <princeyadav00785@gmail.com>
2024-06-28 00:18:01 +0530

    Implemented reset pwd by email id

--- Controller/password.Controller.js
+++ Controller/password.Controller.js
@@ -1,4 +1,3 @@
-// controllers/passwordResetController.js
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const User = require('../Model/usermodel');
@@ -35,13 +34,14 @@ exports.requestPasswordReset = async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
              Please click on the following link, or paste this into your browser to complete the process:\n\n
-             http://${req.headers.host}/reset/${token}\n\n
+             http://${req.headers.host}/api/password/reset/${token}\n\n
              If you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
 
     transporter.sendMail(mailOptions, (err, response) => {
       if (err) {
         console.error('There was an error: ', err);
+        return res.status(500).json({ msg: 'Error sending email' });
       } else {
         res.status(200).json({ msg: 'Recovery email sent' });
       }
@@ -55,6 +55,13 @@ exports.requestPasswordReset = async (req, res) => {
 exports.resetPassword = async (req, res) => {
   const { token } = req.params;
   const { password } = req.body;
+
+  // Log the password to check if it's undefined
+  console.log('Password received:', password);
+
+  if (!password) {
+    return res.status(400).json({ msg: 'Password is required' });
+  }
 
   try {
     const user = await User.findOne({
@@ -75,6 +82,34 @@ exports.resetPassword = async (req, res) => {
     res.status(200).json({ msg: 'Password has been reset' });
   } catch (err) {
     console.error('Error in resetPassword: ', err);
+    res.status(500).send('Server Error');
+  }
+};
+
+exports.displayResetForm = async (req, res) => {
+  const { token } = req.params;
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'Password reset token is invalid or has expired' });
+    }
+
+    // Serve a simple HTML form to reset the password
+    res.send(`
+      <form action="/api/password/reset/${token}" method="POST">
+        <input type="hidden" name="token" value="${token}" />
+        <label for="password">New Password:</label>
+        <input type="password" id="password" name="password" required />
+        <button type="submit">Reset Password</button>
+      </form>
+    `);
+  } catch (err) {
+    console.error('Error in displayResetForm: ', err);
     res.status(500).send('Server Error');
   }
 };
--- Routes/passwordReset.js
+++ Routes/passwordReset.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const passwordController= require('../Controller/password.Controller')
+const passwordResetController= require('../Controller/password.Controller')
 
-router.post('/request-reset',passwordController.requestPasswordReset);
-router.post('/reset/:token',passwordController.resetPassword);
+// Route to request password reset
+router.post('/request-reset', passwordResetController.requestPasswordReset);
+
+// Route to display the reset password form
+router.get('/reset/:token', passwordResetController.displayResetForm);
+
+// Route to reset the password
+router.post('/reset/:token', passwordResetController.resetPassword);
 
 module.exports = router;
